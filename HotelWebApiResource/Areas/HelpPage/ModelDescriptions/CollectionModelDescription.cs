952aa2f862cc0238157b9413e5b77b4baac1e419
Zuhair Haroon Khan <zk222ac@gmail.com>
2019-04-07 00:00:40 +0200

    Add project files.

---
+++ HotelWebApiResource/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace HotelWebApiResource.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ HotelWebApiResource/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace HotelWebApiResource.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
